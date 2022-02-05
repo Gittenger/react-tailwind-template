e89b3ad9833d6d2a743923d22f23c1f9bcce5384
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-02-04 20:32:26 -0500

    init

---
+++ src/components/components.index.js
@@ -0,0 +1,17 @@
+import Button from './General/Button.component.jsx'
+import Footer from './General/Footer.component.jsx'
+import Layout from './General/Layout.component.jsx'
+import Page from './General/Page.Component.jsx'
+import NavList from './General/NavList.component.jsx'
+import NavMobile from './General/NavMobile.component.jsx'
+import StyledCarousel from './General/StyledCarousel.component.jsx'
+export const CIndex = {
+  Button,
+  Footer,
+  Layout,
+  NavList,
+  NavMobile,
+  Page,
+  StyledCarousel,
+}
+export default CIndex
